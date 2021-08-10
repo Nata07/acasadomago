3b8be8b5dc1868b071082aa8bdea14ae24e4892e
Natanael <nata.mw@hotmail.com>
2021-08-09 23:50:35 -0300

    :sparkles: add typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | A Casa do Mago</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  )
+}
