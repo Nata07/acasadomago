fea67c65c770983f5785e2d3c826479969840cf4
Natanael <nata.mw@hotmail.com>
2021-08-12 21:22:10 -0300

    :sparkles: added Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from "next/head";
+import Image from "next/image";
 
+import styled from './home.module.scss';
+import Background from '../../public/images/background6.jpg'
 export default function Home() {
   return (
     <>
       <Head>
         <title>Inicio | A Casa do Mago</title>
       </Head>
-      <h1>Hello World</h1>
+      {/* <div className={styled.background}>
+        <Image src={Background} alt="Background"  />
+
+      </div> */}
+        <main className={styled.container}>
+          <section className={styled.hero}>
+            <span>👏 Hey, bem vindo </span>
+            <h1>A Casa do Mago</h1>
+            <p>Em sintonia com o Universo</p>
+          </section>
+          {/* <img src="https://www.recreio.com.br/images/large/2020/12/11/galaxia-1225945.jpg" alt="Background" /> */}
+        </main>
     </>
   )
 }
