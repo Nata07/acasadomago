4e75e36735038e5c7b2d93af15c9cb145f622c71
Natanael <nata.mw@hotmail.com>
2021-08-12 22:16:48 -0300

    :sparkles: static side generation to price stripe get

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,17 @@ import Image from "next/image";
 
 import styled from './home.module.scss';
 import Background from '../../public/images/background6.jpg'
-export default function Home() {
+import { SubscribeButton } from "../components/SubscribeButton";
+import { GetServerSideProps, GetStaticProps } from "next";
+import { stripe } from "../services/stripe";
+
+interface HomeProps {
+  product: {
+    id: string;
+    amount: number; 
+  }
+}
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -18,9 +28,41 @@ export default function Home() {
             <span>👏 Hey, bem vindo </span>
             <h1>A Casa do Mago</h1>
             <p>Em sintonia com o Universo</p>
+            <p>Receba as nossas novidades <br />
+              <span>por apenas {product.amount}</span>  por mês!
+            </p>
+            <SubscribeButton priceId={product.id} />
           </section>
           {/* <img src="https://www.recreio.com.br/images/large/2020/12/11/galaxia-1225945.jpg" alt="Background" /> */}
         </main>
     </>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JNoIDBh8PT5uuVchrjsrgqO', {
+      expand: ['product']
+    }
+  )
+
+  // const priceFormatted = new Intl.NumberFormat('pt/br', {
+  //   style: 'currency',
+  //   currency: 'BRL'
+  // }).format(price.unit_amount / 100)
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(price.unit_amount / 100),
+  }
+
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+  }
+} 
