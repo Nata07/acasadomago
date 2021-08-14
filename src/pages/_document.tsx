46c45907c89be88d2a15c7e45234a184b418d8b9
Natanael <nata.mw@hotmail.com>
2021-08-14 00:07:56 -0300

    :sparkles: changed fonts project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,8 +6,9 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
+          {/* <link href="https://fonts.googleapis.com/css2?family=STIX+Two+Text:wght@400;600&family=Saira+Condensed:wght@100;500&display=swap" rel="stylesheet"/> */}
           <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;700&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/images/favicon.png" type="image/x-icon" />
         </Head>
         <body>
           <Main />
